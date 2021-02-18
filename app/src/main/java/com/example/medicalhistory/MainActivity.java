ca7f51910905ab02b9ea5f7defd8eeeb12673828
Nico Scha <64054331+jesuisnicolas@users.noreply.github.com>
2021-02-17 22:17:56 -0300

    nicolas

--- app/src/main/java/com/example/medicalhistory/MainActivity.java
+++ app/src/main/java/com/example/medicalhistory/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //Jack was here
+    //No he wasn't
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
