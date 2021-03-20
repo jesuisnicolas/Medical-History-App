51811d669f3c23a8c29dc0cce11a875a51e97f55
Nico Scha <64054331+jesuisnicolas@users.noreply.github.com>
2021-03-20 17:13:06 -0300

    First Activity UI

--- app/src/main/java/com/example/medicalhistory/MainActivity.java
+++ app/src/main/java/com/example/medicalhistory/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.medicalhistory;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -7,6 +8,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -18,11 +23,24 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = "MainActivity";
     private List<Page> pages;
 
+    //container for the patients
+    ViewGroup patientsContainer;
+
+    //variables to create the dialog to create a new patient
+    private AlertDialog.Builder patientInput;
+    private AlertDialog dialog;
+    private EditText patientPopup_firstName, patientPopup_lastName, patientPopup_birthday, patientPopup_condition;
+    private Button patientPopup_cancel, patientPopup_save;
+
+    private Patient newPatient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadData(this);
+
+        patientsContainer = findViewById(R.id.patientsContainer);
     }
 
     public void importPages(){}
@@ -54,5 +72,56 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         saveData(this);
     }
+
+//    public void addPatient(View view) {
+//        TextView patient = new TextView(this);
+//        patient.setText("New Patient");
+//        patientsContainer.addView(patient);
+//    }
+
+    public void createNewPatient(View view){
+        patientInput = new AlertDialog.Builder(this);
+        final View patientPopupView = getLayoutInflater().inflate(R.layout.popup, null);
+
+        patientPopup_firstName = (EditText) patientPopupView.findViewById(R.id.newPatientFirstName);
+        patientPopup_lastName = (EditText) patientPopupView.findViewById(R.id.newPatientLastName);
+//        patientPopup_birthday = (EditText) patientPopupView.findViewById(R.id.newPatientBirthday);
+        patientPopup_condition = (EditText) patientPopupView.findViewById(R.id.newPatientCondition);
+        patientPopup_cancel = (Button) patientPopupView.findViewById(R.id.newPatientCancelBtn);
+        patientPopup_save = (Button) patientPopupView.findViewById(R.id.newPatientSaveBtn);
+
+        patientInput.setView(patientPopupView);
+        dialog = patientInput.create();
+        dialog.show();
+
+        patientPopup_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        patientPopup_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newPatient = new Patient(patientPopup_firstName, patientPopup_lastName, patientPopup_condition);
+                addPatient();
+//                TextView patient;
+//                patient = new TextView(v);
+//                patient.setText(newPatient.getFirstName());
+//                patientsContainer.addView(patient);
+                dialog.dismiss();
+            }
+
+        });
+    }
+
+    public void addPatient() {
+        TextView patient = new TextView(this);
+        patient.setText(newPatient.getFirstName());
+        patientsContainer.addView(patient);
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/medicalhistory/Patient.java
@@ -0,0 +1,59 @@
+package com.example.medicalhistory;
+
+import android.widget.EditText;
+
+import java.util.Date;
+
+public class Patient {
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    private String firstName;
+    private String lastName;
+    private Date birthday; //Do we know how to use this?
+    private String condition;
+
+    //constructor
+//    public Patient(String fName, String lName, Date birthday, String condition){
+//        this.firstName = fName;
+//        this.lastName = lName;
+//        this.birthday = birthday;
+//        this.condition = condition;
+//    }
+
+    public Patient(EditText patientPopup_firstName, EditText patientPopup_lastName, EditText patientPopup_condition) {
+        this.firstName = String.valueOf(patientPopup_firstName);
+        this.lastName = String.valueOf(patientPopup_lastName);
+        this.condition = String.valueOf(patientPopup_condition);
+    }
+}
