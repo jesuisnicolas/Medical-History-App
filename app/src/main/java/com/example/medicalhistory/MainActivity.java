93e0be7da82c206279adcd8f04e69b5f011e443f
popcicleman09 <jack@notunique.com>
2021-04-02 23:11:44 -0600

    fixed error on loading without any patients

--- app/src/main/java/com/example/medicalhistory/MainActivity.java
+++ app/src/main/java/com/example/medicalhistory/MainActivity.java
@@ -91,10 +91,11 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "loaded patients: " + json);
         Gson gson = new Gson();
         String[] names = gson.fromJson(json, String[].class);
-        for(int i = 0; i < names.length; i++){
-            patients.add(new Patient(names[i]));
+        if(names != null) {
+            for (int i = 0; i < names.length; i++) {
+                patients.add(new Patient(names[i]));
+            }
         }
-
         //TODO: this should be a function but im lazy
         ArrayAdapter<String> patientAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
 
