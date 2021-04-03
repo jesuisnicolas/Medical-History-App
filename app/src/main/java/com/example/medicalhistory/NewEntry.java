1ac90105c585ade312574a3ac34e49a690f721b6
Nico Scha <64054331+jesuisnicolas@users.noreply.github.com>
2021-04-03 20:58:04 -0300

    tried to make the file-select to work. Failed miserably

--- app/src/main/java/com/example/medicalhistory/NewEntry.java
+++ app/src/main/java/com/example/medicalhistory/NewEntry.java
@@ -4,21 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.media.VolumeShaper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class NewEntry extends AppCompatActivity {
-/*Here's where the user add a new Entry to the Medical History.
-Each new entry should be stored in an Array linked to the specific user (patient)
-who created it
- */
+    private static final String TAG = "New Entry Activity";
+    /*Here's where the user add a new Entry to the Medical History.
+    Each new entry should be stored in an Array linked to the specific user (patient)
+    who created it
+     */
     public String patientWorkingDir;
     public String filePath;
+    private TextView newEntryFilePath;
     Button selectFileBtn;
+
+    //this is supposed to work to select a file in the phone
+    //but I still don't know how to retrieve the path to the file.
+    final int PICKFILE_REQUEST_CODE = 89; //This will be used by the function that looks for the file URI
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +39,38 @@ who created it
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         patientWorkingDir = message;
 
-
-        //this is supposed to work to select a file in the phone
-        //but I still don't know how to retrieve the path to the file.
-        final int PICKFILE_REQUEST_CODE = 8778;
         selectFileBtn = findViewById(R.id.selectFileBtn);
+        newEntryFilePath = findViewById(R.id.newEntryFilePath);
         selectFileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("file/*");
-                startActivityForResult(intent, PICKFILE_REQUEST_CODE);
-                filePath = ""; //here the path to the file should be assigned.
+                callChooseFileFromDevice();
             }
         });
+    }
+
+    private void callChooseFileFromDevice() {
+        Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+        chooseFile.addCategory(Intent.CATEGORY_OPENABLE);
+
+        chooseFile.setType("*/*");
+        startActivityForResult(chooseFile, PICKFILE_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == PICKFILE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            //the result data contains a URI for the document or directory the user selected
+            if (resultData != null) {
+
+                Log.d(TAG, "onCreate: " + resultData.getData());
+                Intent newIntent = new Intent();
+                newIntent.putExtra(Intent.EXTRA_LOCAL_ONLY, String.valueOf(resultData.getData()));
+                setResult(RESULT_OK, newIntent);
+                finish();
+                newEntryFilePath.setText(resultData.getData().toString());
+            }
+        }
     }
 
     /**
@@ -66,3 +94,4 @@ who created it
 
     }
 }
+
